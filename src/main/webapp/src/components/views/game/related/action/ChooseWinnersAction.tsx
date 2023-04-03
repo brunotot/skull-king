0df80633c722d4d9d200699766b9d8e854ae15a5
crorisvanjski4 <crorisvanjski4@srce.hr>
2023-04-03 17:41:01 +0200

    add mui slider as input for expected winners

--- src/main/webapp/src/components/views/game/related/action/ChooseWinnersAction.tsx
+++ src/main/webapp/src/components/views/game/related/action/ChooseWinnersAction.tsx
@@ -1,35 +1,57 @@
-import Button from "../../../../ui/Button";
-import {Round} from "../GameStats";
+import { Round } from "../GameStats";
 import GameStateService from "../../../../../services/GameStateService";
-import {useContext} from "react";
-import {GameContext} from "../../GameView";
+import { useContext, useState } from "react";
+import { GameContext } from "../../GameView";
+import { Box } from "@mui/system";
+import { Button, Slider } from "@mui/material";
 
 export type ChooseWinnersActionProps = {
-    round: Round;
-}
+	round: Round;
+};
 
-export default function ChooseWinnersAction({ round }: ChooseWinnersActionProps) {
-    const { gameState } = useContext(GameContext);
-    const { id } = gameState;
+export default function ChooseWinnersAction({
+	round,
+}: ChooseWinnersActionProps) {
+	const [value, setValue] = useState<number>(0);
+	const { gameState } = useContext(GameContext);
+	const { id } = gameState;
+	const marks = [...new Array(round + 1)].map((v, i) => ({
+		value: i,
+		label: i.toString(),
+	}));
 
-    async function onExpectedWinnersChooseClick(expectedWinners: number) {
-        await GameStateService.executeAction(id, expectedWinners);
-    }
+	const handleSliderChange = (event: Event, newValue: number | number[]) => {
+		setValue(newValue as number);
+	};
 
-    return <div className="w-full h-full flex flex-col gap-4 items-center justify-center rotate-90">
-        <div className="text-2xl font-bold text-white">
-            Choose number of expected winners
-        </div>
-        <div className="flex flex-wrap gap-4 px-8 justify-center">
-            {[...new Array(round + 1)].map((v, i) => (
-                <Button
-                    onClick={() => onExpectedWinnersChooseClick(i)}
-                    classNameAppend="px-8 font-bold text-2xl"
-                    tailwindColor="green"
-                    text={i.toString()}
-                    key={i}
-                />
-            ))}
-        </div>
-    </div>
+	async function submit() {
+		await GameStateService.executeAction(id, value);
+	}
+
+	return (
+		<div className="w-[98cqi] h-full flex flex-col gap-4 items-center justify-center">
+			<div className="font-bold text-white mb-4">
+				Choose number of expected winners
+			</div>
+			<div className="flex flex-col gap-4">
+				<Box width="100%" sx={{ width: "80cqi" }}>
+					<Slider
+						aria-label="Always visible"
+						step={1}
+						marks={marks}
+						valueLabelDisplay="on"
+						max={round}
+						color="primary"
+						value={value}
+						onChange={handleSliderChange}
+					/>
+				</Box>
+				<div>
+					<Button onClick={() => submit()} variant="contained">
+						Confirm ({value})
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 }
